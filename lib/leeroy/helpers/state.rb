05558019a41e00b8c67546891c5657cfc77a11e3
Steve Huff <steve.huff@runkeeper.com>
2016-01-13 14:43:05 -0500

    saving works

--- lib/leeroy/helpers/state.rb
+++ lib/leeroy/helpers/state.rb
@@ -1,4 +1,5 @@
 require 'hashie'
+require 'json'
 require 'tempfile'
 
 require 'leeroy/helpers'
@@ -15,12 +16,70 @@ module Leeroy
         state.to_s
       end
 
+      def to_json
+        state.to_json
+      end
+
       def respond_to?(method)
         state.respond_to?(method.to_sym) || super
       end
 
       def method_missing(method, *args, &blk)
         state.send(method.to_sym, *args, &blk)
+      end
+
+      def load(file = statefile)
+        begin
+          File.open(file, 'r') do |f|
+            json = _from_json(f.gets)
+          end
+        rescue StandardError => e
+          raise e
+        end
+
+        begin
+          state = json
+        rescue StandardError => e
+          raise e
+        end
+
+        json
+      end
+
+      def save(file = statefile)
+        begin
+          File.new(file, 'w')
+        rescue StandardError => e
+          raise e
+        end
+
+        begin
+          File.open(file, 'w') do |f|
+            f.puts _to_json(state)
+          end
+        rescue StandardError => e
+          raise e
+        end
+
+        file
+      end
+
+      private
+
+      def _to_json(input)
+        begin
+          json = input.to_json
+        rescue StandardError => e
+          raise e
+        end
+      end
+
+      def _from_json(input)
+        begin
+          JSON.parse(input)
+        rescue StandardError => e
+          raise e
+        end
       end
     end
   end
