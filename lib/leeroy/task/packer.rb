61ad70d271a065b07f1d433d8b5da784c7e48ff4
Tim Hartmann <tim.hartmann@runkeeper.com>
2016-11-03 17:28:09 -0400

    Adding a Type and checking for inputs
    PLAT-26 #in-progress
    PLAT-26 #comment created a Packer Type Class
    
    PLAT-24 #comment update the task to use the new type

--- lib/leeroy/task/packer.rb
+++ lib/leeroy/task/packer.rb
@@ -3,6 +3,8 @@ require 'leeroy/task'
 require 'leeroy/helpers/aws'
 require 'leeroy/helpers/inventory'
 require 'leeroy/helpers/packer'
+require 'leeroy/types/packer'
+
 # input
 # pass in path to packer template - ex rk-bastion/main.json
 # LEEROY_APP_NAME=rk-bastion
@@ -25,10 +27,19 @@ module Leeroy
           phase = Leeroy::Types::Phase.resolve(self.state.fetch('phase'), options[:phase])
           logger.debug "phase: #{phase}"
           self.state.phase = phase
+          packer_vars = Leeroy::Types::Packer.new
+
+          if self.state.imageid?
+            imageid = self.state.imageid
+          elsif options[:imageid].nil?
+            imageid = checkEnv('LEEROY_AWS_LINUX_AMI')
+          else
+            imageid = options[:imageid]
+          end
 
           packer_vars = {
             :app_name      => checkEnv('LEEROY_APP_NAME'),
-            :aws_linux_ami => checkEnv('LEEROY_AWS_LINUX_AMI'),
+            :aws_linux_ami => imageid,
             :aws_region    => ENV['AWS_REGION']
           }
           cwd = File.join(checkEnv('LEEROY_PACKER_TEMPLATE_PREFIX'), checkEnv('LEEROY_APP_NAME'))
@@ -37,9 +48,9 @@ module Leeroy
 
           build = buildPacker(cwd,{ :vars => packer_vars } )
           build.artifacts.each do | item |
-            state.message = item.string
+            self.state.message = item.string
             artifact = item.id.split(':')
-            state.imageid = artifact[1]
+            self.state.imageid = artifact[1]
           end
 
           logger.debug "#{build.stdout}"
