7ffee8291fa6de43f3d7b96c6bee78ce1b1169b6
Tim Hartmann <tim.hartmann@runkeeper.com>
2017-02-01 11:47:13 -0500

    Test for new static asset task
    PLAT-224 #comment starting work on adding this to leeroy

---
+++ spec/task/static_assets_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'leeroy'
+require 'leeroy/task'
+require 'leeroy/task/packer'
+require 'leeroy/helpers/aws'
+require 'leeroy/helpers/inventory'
+
+describe "Test Leeroy::Task::Static_assets New" do
+  before(:each) do
+    @sa = Leeroy::Task::Static_assets.new
+  end
+  it "get instance of Leeroy::Task::Static_assets" do
+    expect(@sa).to be_instance_of(Leeroy::Task::Static_assets)
+  end
+  # Test that perform validates packer template syntax
+
+end
