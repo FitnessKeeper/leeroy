e71aba7a14e3ec0796cf2e6006a590e3e0264804
Steve Huff <steve.huff@runkeeper.com>
2017-03-28 10:20:27 -0400

    skip the boilerplate aruba test

--- spec/use_aruba_with_rspec_spec.rb
+++ spec/use_aruba_with_rspec_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe 'version', :type => :aruba do
-  @announce
-  let(:file) { 'file.txt' }
-  let(:content) { 'Hello World' }
+  skip "is boilerplate" do
+    @announce
+    let(:file) { 'file.txt' }
+    let(:content) { 'Hello World' }
 
-  before(:each) { write_file file, content }
+    before(:each) { write_file file, content }
 
-  it { expect(read(file)).to eq [content] }
+    it { expect(read(file)).to eq [content] }
+  end
 end
